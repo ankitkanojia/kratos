b0c512585a397e520eff98d0f784975ff7a58e22
suchitkhunt <suchit.devdigital@gmail.com>
2019-07-23 18:09:21 +0530

    .

--- contracts/KratosToken.sol
+++ contracts/KratosToken.sol
@@ -1,13 +1,36 @@
 pragma solidity ^0.5.0;
 
 contract KratosToken {
-    // constructor
-    // set the total number of tokens
-    // read the total number of tokens
-
+    string public name = "Kratos Token";
+    string public symbol = "Kratos";
+    string public standard = "Kratos Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
+
+    // transfer
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        // exception if account doesn't have enough
+        require(balanceOf[msg.sender] >= _value);
+        // transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const KratosToken = artifacts.require("./KratosToken.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(KratosToken);
+    deployer.deploy(KratosToken, 1000000);
 };
