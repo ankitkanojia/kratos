c55df22a741a8331d3ee8f87cbc9af726298da27
suchitkhunt <suchit.devdigital@gmail.com>
2019-07-24 09:59:22 +0530

    .

--- contracts/KratosToken.sol
+++ contracts/KratosToken.sol
@@ -12,25 +12,48 @@ contract KratosToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
-    // transfer
     function transfer(address _to, uint256 _value) public returns (bool success){
-        // exception if account doesn't have enough
         require(balanceOf[msg.sender] >= _value);
-        // transfer the balance
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
-        // transfer event
         emit Transfer(msg.sender, _to, _value);
-
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
 
 }
