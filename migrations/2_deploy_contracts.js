a84370b1493e96ab669acfa65c1ead23db07e634
Suchit <khuntsr@gmail.com>
2019-07-26 14:55:32 +0530

    .

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const KratosToken = artifacts.require("./KratosToken.sol");
+const KratosTokenSale = artifacts.require("./KratosTokenSale.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(KratosToken, 1000000);
+    deployer.deploy(KratosToken, 1000000).then(function() {
+        // Token price is 0.001 Ether
+        var tokenPrice = 1000000000000000;
+        return deployer.deploy(KratosTokenSale, KratosToken.address, tokenPrice);
+    });
 };
+
+
